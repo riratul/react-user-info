75d9f87937b9b6ef16e12db16e9559f5bfcd5326
Rayatul Islam <rayatislam@gmail.com>
2021-10-01 10:32:03 +0200

    Login  Button updated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -13,7 +13,9 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/login" className="btn btn-primary">
+              Login
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
