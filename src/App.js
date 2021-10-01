c9a403288d82a07e3f85ff14b9699558609f76ca
Rayatul Islam <rayatislam@gmail.com>
2021-10-01 12:51:02 +0200

    Home component added
    Home component added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import NavBar from './components/NavBar/NavBar';
-import AllPosts from './components/AllPosts/AllPosts';
-import Login from './components/Login/Login';
-import AddPost from './components/AddPost/AddPost';
-import EditPost from './components/EditPost/EditPost';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import AllPosts from "./components/AllPosts/AllPosts";
+import Login from "./components/Login/Login";
+import AddPost from "./components/AddPost/AddPost";
+import EditPost from "./components/EditPost/EditPost";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <NavBar />
-          <Switch>            
-            <Route path="/login" component={Login} />
-            <Route path="/add" component={AddPost} />
-            <Route path="/edit/:id" component={EditPost} />
-            <Route path="/allposts" component={AllPosts} />
-          </Switch>
+        <NavBar />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/add" component={AddPost} />
+          <Route path="/edit/:id" component={EditPost} />
+          <Route path="/allposts" component={AllPosts} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <>
+      <h2>Please login to see all the posts</h2>
+    </>
+  );
+}
+
+export default Home;
